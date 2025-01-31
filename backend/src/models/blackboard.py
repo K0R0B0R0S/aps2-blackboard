9c4bea9d763e34b74b757c3a86c11fa73f4c486e
Raphael Kenji <115382121+raphaelkenji@users.noreply.github.com>
2025-01-30 23:56:10 -0300

    blackboard docs, move index, fix vendas product listing

--- backend/src/models/blackboard.py
+++ backend/src/models/blackboard.py
@@ -5,67 +5,190 @@ class Blackboard:
     _instance = None
 
     def __new__(cls, db_session=None):
-        """Método para garantir que só haverá uma instância de Blackboard (implementação do SINGLETON).
+        """
+        Método para garantir que só haverá uma instância de Blackboard (implementação do SINGLETON).
         
         Esse método é responsável por garantir que apenas uma instância da classe Blackboard seja criada. 
         Se já existe uma instância, ele simplesmente retorna essa instância. 
         Caso contrário, ele cria uma nova instância e a armazena no atributo estático _instance.
         
         """
+        
         if cls._instance is None:
             cls._instance = super().__new__(cls)  # Cria a instância
             cls._instance.db_session = db_session  # Inicializa a sessão de banco de dados
         return cls._instance  # Retorna a instância única
 
     def __init__(self, db_session=None):
-        """Inicia a instância do Blackboard com uma sessão de banco de dados, se necessário."""
+        """
+        Inicia a instância do Blackboard com uma sessão de banco de dados, se necessário.
+        """
+        
         if db_session is not None:
             self.db = db_session
 
     # CRUD para Loja
     def adicionar_loja(self, nome_loja: str, endereco: str, telefone: str):
+        """
+        Adiciona uma nova loja ao banco de dados.
+
+        Args:
+            nome_loja (str): O nome da loja a ser adicionada.
+            endereco (str): O endereço da loja.
+            telefone (str): O telefone de contato da loja.
+
+        Returns:
+            Loja: A instância da loja recém-adicionada.
+        """
+
         nova_loja = Loja(nome_loja=nome_loja, endereco=endereco, telefone=telefone)
         self.db.add(nova_loja)
         self.db.commit()
         self.db.refresh(nova_loja)
         return nova_loja
+    
+    def atualizar_loja(self, loja_id: int, nome_loja: str, endereco: str, telefone: str):
+        """
+        Atualiza os dados de uma loja no banco de dados.
+
+        Args:
+            loja_id (int): O ID da loja a ser atualizada.
+            nome_loja (str): O novo nome da loja.
+            endereco (str): O novo endereço da loja.
+            telefone (str): O novo telefone da loja.
+
+        Returns:
+            Loja: A loja atualizada, se a atualização for bem-sucedida.
+            None: Se a loja com o ID fornecido não for encontrada.
+        """
+        
+        loja: Loja = self.obter_loja(loja_id)
+        if loja:
+            loja.nome_loja = nome_loja
+            loja.endereco = endereco
+            loja.telefone = telefone
+            self.db.commit()
+            self.db.refresh(loja)
+            return loja
+        return
 
     def obter_loja(self, loja_id: int):
+        """
+        Obtém uma loja do banco de dados com base no ID fornecido.
+
+        Args:
+            loja_id (int): O ID da loja a ser obtida.
+
+        Returns:
+            Loja: A loja correspondente ao ID fornecido, ou None se não for encontrada.
+        """
+        
         return self.db.query(Loja).filter(Loja.id_loja == loja_id).first()
 
     def listar_lojas(self):
+        """
+        Lista todas as lojas do banco de dados.
+
+        Returns:
+            list: Uma lista de objetos Loja representando todas as lojas no banco de dados.
+        """
+        
         return self.db.query(Loja).all()
 
     # CRUD para Produto
     def adicionar_produto(self, nome_produto: str, descricao: str, preco_unitario: float, estoque_minimo: int):
+        """
+        Adiciona um novo produto ao banco de dados.
+
+        Args:
+            nome_produto (str): O nome do produto a ser adicionado.
+            descricao (str): A descrição do produto.
+            preco_unitario (float): O preço unitário do produto.
+            estoque_minimo (int): A quantidade mínima de estoque do produto.
+
+        Returns:
+            Produto: O objeto Produto recém-adicionado ao banco de dados.
+        """
+        
         novo_produto = Produto(nome_produto=nome_produto, descricao=descricao, preco_unitario=preco_unitario, estoque_minimo=estoque_minimo)
         self.db.add(novo_produto)
         self.db.commit()
         self.db.refresh(novo_produto)
         return novo_produto
+    
+    def atualizar_produto(self, produto_id: int, nome_produto: str, descricao: str, preco_unitario: float, estoque_minimo: int):
+        """
+        Atualiza os dados de um produto no banco de dados.
+        Args:
+            produto_id (int): O ID do produto.
+            nome_produto (str): O nome do produto.
+            descricao (str): A descrição do produto.
+            preco_unitario (float): O preço unitário do produto.
+            estoque_minimo (int): O estoque mínimo do produto.
+        Returns:
+            Produto: O objeto de produto atualizado.
+        """
+        
+        produto: Produto = self.obter_produto(produto_id)
+        if produto:
+            produto.nome_produto = nome_produto
+            produto.descricao = descricao
+            produto.preco_unitario = preco_unitario
+            produto.estoque_minimo = estoque_minimo
+            self.db.commit()
+            self.db.refresh(produto)
+            return produto
+        return
 
     def obter_produto(self, produto_id: int):
+        """
+        Obtém um produto do banco de dados.
+        Args:
+            produto_id (int): O ID do produto.
+        Returns:
+            Produto: O objeto de produto encontrado.
+        """
+        
         return self.db.query(Produto).filter(Produto.id_produto == produto_id).first()
 
     def listar_produtos(self):
+        """
+        Lista todos os produtos do banco de dados.
+        Returns:
+            list: Uma lista de objetos Produto representando todos os produtos.
+        """
+        
         return self.db.query(Produto).all()
 
     # Operações no Estoque
     def adicionar_estoque(self, loja_id: int, produto_id: int, quantidade: int):
+        """
+        Adiciona um novo item de estoque ao banco de dados.
+        Args:
+            loja_id (int): O ID da loja.
+            produto_id (int): O ID do produto.
+            quantidade (int): A quantidade a ser adicionada ao estoque.
+        Returns:
+            Estoque: O objeto de estoque adicionado.
+        """
+        
         estoque = Estoque(id_loja=loja_id, id_produto=produto_id, quantidade=quantidade)
         self.db.add(estoque)
         self.db.commit()
         self.db.refresh(estoque)
         return estoque
-
-    def obter_estoque(self, loja_id: int, produto_id: int):
-        return self.db.query(Estoque).filter(Estoque.id_loja == loja_id, Estoque.id_produto == produto_id).first()
     
-    def listar_estoque_por_produto(self, produto_id: int):
-        """Retorna o estoque de um produto em todas as lojas."""
-        return self.db.query(Estoque).filter(Estoque.id_produto == produto_id).all()
-
     def atualizar_estoque(self, loja_id: int, produto_id: int, quantidade: int):
+        """
+        Atualiza a quantidade de um item de estoque no banco de dados.
+        Args:
+            loja_id (int): O ID da loja.
+            produto_id (int): O ID do produto.
+            quantidade (int): A quantidade a ser adicionada ao estoque.
+        Returns:
+            Estoque: O objeto de estoque atualizado.
+        """
+        
         estoque = self.obter_estoque(loja_id, produto_id)
         if estoque:
             estoque.quantidade += quantidade
@@ -74,8 +197,40 @@ class Blackboard:
             return estoque
         return None
 
+    def obter_estoque(self, loja_id: int, produto_id: int):
+        """
+        Obtém um item de estoque do banco de dados.
+        Args:
+            loja_id (int): O ID da loja.
+            produto_id (int): O ID do produto.
+        Returns:
+            Estoque: O objeto de estoque encontrado.
+        """
+        
+        return self.db.query(Estoque).filter(Estoque.id_loja == loja_id, Estoque.id_produto == produto_id).first()
+    
+    def listar_estoque_por_produto(self, produto_id: int):
+        """
+        Retorna o estoque de um produto em todas as lojas.
+        Args:
+            produto_id (int): O ID do produto.
+        Returns:
+            list: Uma lista de objetos Estoque representando o estoque do produto em todas as lojas.
+        """
+        
+        return self.db.query(Estoque).filter(Estoque.id_produto == produto_id).all()
+
     # Operações de Venda
-    def registrar_venda(self, loja_id: int, produtos: list):  # produtos é uma lista de tuplas (produto_id, quantidade)
+    def registrar_venda(self, loja_id: int, produtos: list):
+        """
+        Registra uma venda no sistema.
+        Args:
+            loja_id (int): O ID da loja.
+            produtos (list): Uma lista de tuplas contendo (produto_id, quantidade, preco_unitario).
+        Returns:
+            Venda: O objeto de venda registrado.
+        """
+        
         total_venda = sum([produto[1] * produto[2] for produto in produtos])  # Calculando o valor total da venda
         venda = Venda(id_loja=loja_id, valor_total=total_venda)
         self.db.add(venda)
@@ -89,9 +244,34 @@ class Blackboard:
             self.db.commit()
 
         return venda
+    
+    def listar_vendas(self):
+        """
+        Retorna uma lista de todas as vendas do banco de dados.
+        Returns:
+            list: Uma lista de objetos Venda representando todas as vendas.
+        """
+        
+        vendas = self.db.query(Venda).order_by(Venda.data_venda.desc()).all()
+        for venda in vendas:
+            venda.itens = ', '.join([
+            f"{self.obter_produto(item.id_produto).nome_produto} (Qtd: {item.quantidade})"
+            for item in self.db.query(ItemVenda).filter(ItemVenda.id_venda == venda.id_venda).all()
+            ])
+        return vendas
 
     # Operações de Compra
-    def registrar_compra(self, fornecedor_id: int, produtos: list):  # produtos é uma lista de tuplas (produto_id, quantidade, preco_unitario)
+    
+    def registrar_compra(self, fornecedor_id: int, produtos: list):
+        """
+        Registra uma compra no sistema.
+        Args:
+            fornecedor_id (int): O ID do fornecedor.
+            produtos (list): Uma lista de tuplas contendo (produto_id, quantidade, preco_unitario).
+        Returns:
+            Compra: O objeto de compra registrado.
+        """
+        
         valor_total = sum([produto[1] * produto[2] for produto in produtos])  # Calculando o valor total da compra
         compra = Compra(id_fornecedor=fornecedor_id, valor_total=valor_total)
         self.db.add(compra)
@@ -106,10 +286,11 @@ class Blackboard:
 
         return compra
 
-    # Relacionamentos entre produtos e compras
     def listar_compras(self):
-        return self.db.query(Compra).all()
-
-    def listar_vendas(self):
-        return self.db.query(Venda).all()
-
+        """
+        Retorna uma lista de todas as compras do banco de dados.
+        Retorna:
+            list: Uma lista de objetos Compra representando todas as compras.
+        """
+        
+        return self.db.query(Compra).order_by(Compra.data_compra.desc()).all()
